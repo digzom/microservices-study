9e68a6c0569b2313954a017bd5c77256171c4415
Dickson <dicksonmelo@gmail.com>
2022-10-04 05:39:14 -0300

    finishing first part

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -1,26 +1,11 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchData = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-
-    setComments(res.data);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
-  });
-
-  return <ul>{renderedComments}</ul>;
-};
+const CommentList = ({ comments }) => (
+  <ul>
+    {comments?.map((comment) => {
+      return <li key={comment.id}>{comment.content}</li>;
+    })}
+  </ul>
+);
 
 export default CommentList;
--- blog/query/index.js
+++ blog/query/index.js
@@ -15,25 +15,17 @@ app.get("/posts", (req, res) => {
 
 app.post("/events", (req, res) => {
   const { type, data } = req.body;
-  console.log("Body: ", req.body);
 
   if (type === "PostCreated") {
     const { id, title } = data;
-    console.log("Data: ", data);
 
     posts[id] = { id, title, comments: [] };
-    console.log(posts);
   }
 
   if (type === "CommentCreated") {
     const { id, content, postId } = data;
 
-    console.log("Data: ", data);
-    console.log("Posts: ", posts);
-
     const post = posts[postId];
-    console.log("Post: ", post);
-
     post.comments.push({ id, content });
   }
 
